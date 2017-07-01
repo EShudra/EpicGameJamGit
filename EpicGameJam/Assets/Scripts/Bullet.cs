a435706b96654a9bd26fcc687c6a2b64624c9e6d
EShudra <poorgenmail@rambler.ru>
2017-07-01 13:30:47 +0300

    Gun and Bullet
    base work

---
+++ EpicGameJam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	//bullet moving speed
+	public float bulletSpeed;
+
+	//bullet damage
+	public float damage;
+
+	//true if bullet pass through enemies
+	public bool piercing;
+
+	//move bullet formard
+	void Move(){
+		this.transform.Translate (new Vector3(bulletSpeed*Time.deltaTime, 0, 0));	
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+}
