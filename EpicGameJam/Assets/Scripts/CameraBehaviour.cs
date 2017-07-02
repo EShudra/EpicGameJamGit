0ee141ccfc085428e9c812de9dc3c473da128d3e
Anglain <shudra.ia@gmail.com>
2017-07-02 06:15:50 +0300

    Merge branch 'master' into Anglain

---
+++ EpicGameJam/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			anim.SetTrigger ("Shake");
+		}
+	}
+
+	public void shakeScreen(){
+		anim.SetTrigger ("Shake");
+	}
+
+}
