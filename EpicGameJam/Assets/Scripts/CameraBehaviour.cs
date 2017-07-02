7e4396b535cf7f1b4a4f7a5a693bc5df30571c48
EShudra <poorgenmail@rambler.ru>
2017-07-02 06:06:28 +0300

    questions update

--- EpicGameJam/Assets/Scripts/CameraBehaviour.cs
+++ EpicGameJam/Assets/Scripts/CameraBehaviour.cs
@@ -11,9 +11,9 @@ public class CameraBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
+		/*if (Input.GetMouseButtonDown (0)) {
 			anim.SetTrigger ("Shake");
-		}
+		}*/
 	}
 
 	public void shakeScreen(){
---
+++ EpicGameJam/Assets/Scripts/Question.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Question : MonoBehaviour {
+
+	//places for answers
+	public GameObject[] answerPlacers;
+
+	//DRAG & DROP answer library
+	public AnswersLibrary aLib;
+
+	Answer[] ansPrefabs = new Answer[3];
+
+	// Use this for initialization
+	void Start () {
+		ansPrefabs = aLib.GetAnswers ();
+		Instantiate (ansPrefabs[0],answerPlacers[0].transform.position,Quaternion.identity,this.transform);
+		Instantiate (ansPrefabs[1],answerPlacers[1].transform.position,Quaternion.identity,this.transform);
+		Instantiate (ansPrefabs[2],answerPlacers[2].transform.position,Quaternion.identity,this.transform);
+	}
+
+	void OnDestroy(){
+		//this.transform.GetComponentInParent<QuestionController> ().NextQuestion ();
+		this.transform.parent.gameObject.GetComponent<QuestionController>().NextQuestion();
+	}
+	
+
+}
