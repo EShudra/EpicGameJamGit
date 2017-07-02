75b038e0fedc3e1d5b5651774cb4dbb7624be97c
EShudra <poorgenmail@rambler.ru>
2017-07-02 10:55:28 +0300

    princess

--- EpicGameJam/Assets/Scripts/ExitPoint.cs
+++ EpicGameJam/Assets/Scripts/ExitPoint.cs
@@ -3,21 +3,41 @@ using System.Collections;
 
 public class ExitPoint : MonoBehaviour {
 
-	//time from level start when exit will opens(in seconds)
+	//time from this created when exit will opens(in seconds)
 	public float timeWhenOpens;
 
 	//enabled(you can exit if you want)
 	public bool exitOpened = false;
 
+	//D&D
+	public GameObject Princess;
+	bool PrincessIsOut = false;
+
+	float birthTime = 0;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		birthTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (timeWhenOpens < Time.time - birthTime ){
+			this.gameObject.GetComponent<Animator> ().SetBool ("active", true);	
+
+		}
+
+		if ((!PrincessIsOut) && (timeWhenOpens + 0.7f < Time.time - birthTime)) {
+			PrincessIsOut = true;
+			Princess.gameObject.SetActive(true);
+			exitOpened = true;
+			//object o = Instantiate (PrincessPrefab, PrincessPrefab.transform.position, Quaternion.identity, this.transform);
+			//GameObject go = o as GameObject;
+			//go.transform.localScale = PrincessPrefab.transform.localScale;
+
+		}
+
 	}
 
 	void OnTriggerStay2D(Collider2D enteringCollider){
