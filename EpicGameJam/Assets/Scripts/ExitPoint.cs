9a1308f2b03b33517862769cc7d5aabcc26ddfbc
Anglain <shudra.ia@gmail.com>
2017-07-02 23:18:15 +0300

    Update
    ExitPoint prefab
    More sounds added
    Enemy on hit animations

--- EpicGameJam/Assets/Scripts/ExitPoint.cs
+++ EpicGameJam/Assets/Scripts/ExitPoint.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class ExitPoint : MonoBehaviour {
 
+
+	public AudioClip doorOpening;
+	public AudioClip doorOverTime;
+	public AudioClip doorInteraction;
+
+	private float lastTimePrincessScreamed;
+	private float princessCooldown = 3f;
 	//
 	public QuestionController qCont;
 
@@ -37,23 +44,32 @@ public class ExitPoint : MonoBehaviour {
 		
 		if (timeWhenOpens < Time.time - birthTime ){
 			this.gameObject.GetComponent<Animator> ().SetBool ("active", true);	
-
+			//SoundManager.instance.PlaySingle (doorOpening);
 		}
 
 		if ((!PrincessIsOut) && (timeWhenOpens + 0.7f < Time.time - birthTime)) {
 			PrincessIsOut = true;
 			Princess.gameObject.SetActive(true);
 			exitOpened = true;
+			//SoundManager.instance.PlaySingle (doorOverTime);
+			//lastTimePrincessScreamed = Time.time;
+
 			//object o = Instantiate (PrincessPrefab, PrincessPrefab.transform.position, Quaternion.identity, this.transform);
 			//GameObject go = o as GameObject;
 			//go.transform.localScale = PrincessPrefab.transform.localScale;
 
 		}
+		/*
+		if (PrincessIsOut && (lastTimePrincessScreamed+princessCooldown)<Time.time) {
+			SoundManager.instance.PlaySingle (doorOverTime);
+			lastTimePrincessScreamed = Time.time;
+		}*/
 
 	}
 
 	void OnTriggerStay2D(Collider2D enteringCollider){
 		if (enteringCollider.tag == "Player"){
+			//SoundManager.instance.PlaySingle (doorInteraction);
 			EndLevel();
 		}
 	}
@@ -61,7 +77,7 @@ public class ExitPoint : MonoBehaviour {
 	void EndLevel(){
 		if (exitOpened) {
 			Destroy (level);
-			qCont.NextQuestion ();
+			//qCont.NextQuestion ();
 			/*foreach (var item in GameObject.FindObjectsOfType<GameObject>()) {
 				if (item.GetComponent<Camera> () == null) {
 					Destroy (item);
--- EpicGameJam/Assets/Scripts/SoundManager.cs
+++ EpicGameJam/Assets/Scripts/SoundManager.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 
-	public AudioSource efxSource;
 	public AudioSource musicSource;
+	public AudioSource efxSource;
+	public AudioSource bulletEfx;
+	public AudioSource bulletEffectsEfx;
+	public AudioSource explosionEfx;
+	public AudioSource enemyEfx;
+	public AudioSource playerEfx;
+
 	public static SoundManager instance = null;
 
 	public float lowPitchRange = 0.95f;
@@ -22,6 +28,43 @@ public class SoundManager : MonoBehaviour {
 	public void PlaySingle (AudioClip clip) {
 		efxSource.clip = clip;
 		efxSource.Play ();
+	}
+
+	public void PlaySingleBullet (AudioClip clip) {
+		bulletEfx.clip = clip;
+		bulletEfx.Play ();
+	}
+
+	public void PlayBulletEffect (AudioClip clip) {
+		bulletEffectsEfx.clip = clip;
+		bulletEffectsEfx.Play ();
+	}
+
+	public void PlayExplosion (params AudioClip[] clips) {
+		int randomIndex = Random.Range (0, clips.Length);
+		float randomPitch = Random.Range (lowPitchRange, highPitchRange);
+
+		explosionEfx.pitch = randomPitch;
+		explosionEfx.clip = clips [randomIndex];
+		explosionEfx.Play ();
+	}
+
+	public void PlayEnemySound (params AudioClip[] clips) {
+		int randomIndex = Random.Range (0, clips.Length);
+		float randomPitch = Random.Range (lowPitchRange, highPitchRange);
+
+		enemyEfx.pitch = randomPitch;
+		enemyEfx.clip = clips [randomIndex];
+		enemyEfx.Play ();
+	}
+
+	public void PlayPlayerSound (params AudioClip[] clips) {
+		int randomIndex = Random.Range (0, clips.Length);
+		float randomPitch = Random.Range (lowPitchRange, highPitchRange);
+
+		playerEfx.pitch = randomPitch;
+		playerEfx.clip = clips [randomIndex];
+		playerEfx.Play ();
 	}
 
 	public void RandomizeSfx (params AudioClip[] clips) {
