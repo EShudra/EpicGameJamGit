fb504082f1109240693d61446d3ec501874b27c6
Anglain <shudra.ia@gmail.com>
2017-07-02 19:37:48 +0300

    Something, explosion changes

--- EpicGameJam/Assets/Scripts/Explosion.cs
+++ EpicGameJam/Assets/Scripts/Explosion.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Explosion : MonoBehaviour {
 
+	public CameraBehaviour cam;
 	public float explosionRadius = 2f;
 	public float explosionDamage = 500f;
+	public AudioClip explosionSound;
+	private Animator anim;
 
 	void Start () {
-		GetComponent<Animator>().Play ("explosion4");
+		anim = GetComponent<Animator> ();
+		anim.Play ("explosion4");
+		GameObject.FindObjectOfType<CameraBehaviour>().shakeScreen ();
+		SoundManager.instance.PlaySingle (explosionSound);
 
 		RaycastHit2D[] hits = Physics2D.CircleCastAll (this.transform.position, explosionRadius, new Vector3(0,0,1));
 		Debug.Log ("hits.Length: "+hits.Length);
