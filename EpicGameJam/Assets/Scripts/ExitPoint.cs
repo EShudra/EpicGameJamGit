3e7ee1e020d007eda51215d166f60f7a97e62d52
Anglain <shudra.ia@gmail.com>
2017-07-03 11:13:20 +0300

    ExitPoint prefab

--- EpicGameJam/Assets/Scripts/ExitPoint.cs
+++ EpicGameJam/Assets/Scripts/ExitPoint.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class ExitPoint : MonoBehaviour {
 
 
+	private AudioSource aus;
 	public AudioClip doorOpening;
 	public AudioClip doorOverTime;
 	public AudioClip doorInteraction;
 
 	private float lastTimePrincessScreamed;
-	private float princessCooldown = 3f;
+	private float princessCooldown = 1.5f;
 	//
 	public QuestionController qCont;
 
@@ -37,6 +38,7 @@ public class ExitPoint : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		birthTime = Time.time;
+		aus = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -44,32 +46,35 @@ public class ExitPoint : MonoBehaviour {
 		
 		if (timeWhenOpens < Time.time - birthTime ){
 			this.gameObject.GetComponent<Animator> ().SetBool ("active", true);	
-			//SoundManager.instance.PlaySingle (doorOpening);
+			///aus.PlayOneShot (doorOpening, 0.7f);
 		}
 
 		if ((!PrincessIsOut) && (timeWhenOpens + 0.7f < Time.time - birthTime)) {
 			PrincessIsOut = true;
 			Princess.gameObject.SetActive(true);
 			exitOpened = true;
-			//SoundManager.instance.PlaySingle (doorOverTime);
-			//lastTimePrincessScreamed = Time.time;
+
+			//aus.PlayOneShot (doorOverTime, 0.7f);
+			lastTimePrincessScreamed = Time.time;
 
 			//object o = Instantiate (PrincessPrefab, PrincessPrefab.transform.position, Quaternion.identity, this.transform);
 			//GameObject go = o as GameObject;
 			//go.transform.localScale = PrincessPrefab.transform.localScale;
 
 		}
-		/*
+
 		if (PrincessIsOut && (lastTimePrincessScreamed+princessCooldown)<Time.time) {
-			SoundManager.instance.PlaySingle (doorOverTime);
+			//aus.PlayOneShot (doorOverTime, 0.7f);
 			lastTimePrincessScreamed = Time.time;
-		}*/
+			Debug.Log ("You heard the clip, didn't you?");
+		}
 
 	}
 
 	void OnTriggerStay2D(Collider2D enteringCollider){
 		if (enteringCollider.tag == "Player"){
-			//SoundManager.instance.PlaySingle (doorInteraction);
+			//aus.PlayOneShot (doorInteraction, 0.7f);
+			Debug.Log ("Intereaction with the princess");
 			EndLevel();
 		}
 	}
